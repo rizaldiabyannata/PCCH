42f525f343c6f2f3f389f1e79982381e12aa7340
rizaldiabyannata <113687598+rizaldiabyannata@users.noreply.github.com>
2022-12-10 21:16:03 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-function changeImageOne(){
+function changeImageOne() {
   var img = document.getElementById("front-image");
   var imgTwo = document.getElementById("back-image");
   img.style.display = "none";
   imgTwo.style.display = "flex";
 }
 
-function changeImageTwo(){
+function changeImageTwo() {
   var img = document.getElementById("back-image");
   var imgTwo = document.getElementById("front-image");
   img.style.display = "none";
   imgTwo.style.display = "flex";
+}
+
+function changeTxt1() {
+  document.getElementById("price1").innerHTML = "BUY NOW";
+}
+function backNormal1() {
+  document.getElementById("price1").innerHTML = "Harga : Rp315,000";
+}
+function changeTxt2() {
+  document.getElementById("price2").innerHTML = "BUY NOW";
+}
+function backNormal2() {
+  document.getElementById("price2").innerHTML = "Harga : Rp340,000";
+}
+function changeTxt3() {
+  document.getElementById("price3").innerHTML = "BUY NOW";
+}
+function backNormal3() {
+  document.getElementById("price3").innerHTML = "Harga : Rp370.000";
 }
