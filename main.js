ec6c336b254ffb4d92f9837206ffe13a3ad26b5d
rizaldiabyannata <113687598+rizaldiabyannata@users.noreply.github.com>
2022-12-16 20:10:34 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -18,15 +18,55 @@ function changeTxt1() {
 function backNormal1() {
   document.getElementById("price1").innerHTML = "Harga : Rp315,000";
 }
+function loading1(){
+  document.getElementById("price1").style.display = "none";
+  document.getElementById("loader1").style.display ="flex";
+}
 function changeTxt2() {
   document.getElementById("price2").innerHTML = "BUY NOW";
 }
 function backNormal2() {
   document.getElementById("price2").innerHTML = "Harga : Rp340,000";
 }
+function loading2(){
+  document.getElementById("price2").style.display = "none";
+  document.getElementById("loader2").style.display ="flex";
+}
 function changeTxt3() {
   document.getElementById("price3").innerHTML = "BUY NOW";
 }
 function backNormal3() {
   document.getElementById("price3").innerHTML = "Harga : Rp370.000";
+}
+function loading3(){
+  document.getElementById("price3").style.display = "none";
+  document.getElementById("loader3").style.display ="flex";
+}
+
+function openNav() {
+  // Get the checkbox and slider bar elements
+  var checkbox = document.getElementById("check");
+  var sliderBar = document.getElementById("sliderBar");
+  var changeColor1 = document.getElementById("line1");
+  var changeColor2 = document.getElementById("line2");
+  var changeColor3 = document.getElementById("line3");
+  var burger = document.getElementById("containerHamburger");
+  // Toggle the checked property of the checkbox
+  checkbox.checked = !checkbox.checked;
+
+  // If the checkbox is unchecked, hide the slider bar
+  if (!checkbox.checked) {
+    sliderBar.style.width = "0px";
+    changeColor1.style.backgroundColor="#333";
+    changeColor2.style.backgroundColor="#333";
+    changeColor3.style.backgroundColor="#333";
+    burger.style.marginLeft = "0px";
+  } else {
+    // Otherwise, show the slider bar
+    sliderBar.style.width = "250px";
+    changeColor1.style.backgroundColor="white";
+    changeColor2.style.backgroundColor="white";
+    changeColor3.style.backgroundColor="white";
+    burger.style.marginLeft = "20px";
+  }
 }
